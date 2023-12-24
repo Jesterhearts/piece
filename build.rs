36b6d6512a016690197a8adc7a6be4d31b8aac5f
Jesterhearts <Jesterhearts@users.noreply.github.com>
2023-12-23 20:02:48 -0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/protos");
 
-    let out_dir_env = std::env::var_os("OUT_DIR").unwrap();
-    let out_dir = std::path::Path::new(&out_dir_env);
-
     protobuf_codegen::Codegen::new()
         .pure()
         // All inputs and imports from the inputs must reside in `includes` directories.
@@ -14,19 +11,6 @@ fn main() {
                 .unwrap()
                 .map(|f| f.unwrap().path()),
         )
-        // Specify output directory relative to Cargo output directory.
-        .cargo_out_dir("protos")
+        .out_dir("src/protogen")
         .run_from_script();
-
-    for path in std::fs::read_dir(out_dir.join("protos"))
-        .unwrap()
-        .map(|f| f.unwrap().path())
-    {
-        let filename = format!(
-            "src/protogen/{}",
-            path.file_name().unwrap().to_string_lossy()
-        );
-        std::fs::File::create(&filename).unwrap();
-        std::fs::copy(path, filename).unwrap();
-    }
 }
