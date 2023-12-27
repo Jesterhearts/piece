2ef3a29818c4007295e889aca19d9f8ed70820e7
Jesterhearts <Jesterhearts@users.noreply.github.com>
2023-12-27 12:56:47 -0800

    update sanguine evangelist

--- src/bin/format_textproto.rs
+++ src/bin/format_textproto.rs
@@ -1,10 +1,12 @@
-use piece::load_protos;
+use piece::{card::Card, load_protos};
 use protobuf::text_format::print_to_string_pretty;
 
 fn main() -> anyhow::Result<()> {
     let cards = load_protos()?;
 
     for (card, card_file) in cards {
+        let _: Card = (&card).try_into()?;
+
         std::fs::write(
             std::path::Path::new("cards").join(card_file.path()),
             print_to_string_pretty(&card),
